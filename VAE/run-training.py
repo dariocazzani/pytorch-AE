ca226740657ee450786abb72c0b9f2a8cbe8e82f
Dario Cazzani <dariocazzani@gmail.com>
2018-11-02 18:30:23 -0700

    Refactored functions in a single training object

--- VAE/run-training.py
+++ VAE/run-training.py
@@ -50,11 +50,9 @@ test_loader = torch.utils.data.DataLoader(
     datasets.MNIST('../data', train=False, transform=transforms.ToTensor()),
     batch_size=args.batch_size, shuffle=True, **kwargs)
 
-
-
-class VAE(nn.Module):
+class Network(nn.Module):
     def __init__(self):
-        super(VAE, self).__init__()
+        super(Network, self).__init__()
 
         self.encoder = CNN_Encoder(args.embedding_size)
         self.decoder = CNN_Decoder(args.embedding_size)
@@ -75,70 +73,72 @@ class VAE(nn.Module):
         z = self.reparameterize(mu, logvar)
         return self.decode(z), mu, logvar
 
+class VAE(object):
+    def __init__(self):
+        self.model = Network()
+        self.model.to(device)
 
-model = VAE()
-model.to(device)
+        self.optimizer = optim.Adam(self.model.parameters(), lr=1e-3)
 
-optimizer = optim.Adam(model.parameters(), lr=1e-3)
+    # Reconstruction + KL divergence losses summed over all elements and batch
+    def loss_function(self, recon_x, x, mu, logvar):
+        BCE = F.binary_cross_entropy(recon_x, x.view(-1, 784), reduction='sum')
 
-# Reconstruction + KL divergence losses summed over all elements and batch
-def loss_function(recon_x, x, mu, logvar):
-    BCE = F.binary_cross_entropy(recon_x, x.view(-1, 784), reduction='sum')
+        # see Appendix B from VAE paper:
+        # Kingma and Welling. Auto-Encoding Variational Bayes. ICLR, 2014
+        # https://arxiv.org/abs/1312.6114
+        # 0.5 * sum(1 + log(sigma^2) - mu^2 - sigma^2)
+        KLD = -0.5 * torch.sum(1 + logvar - mu.pow(2) - logvar.exp())
 
-    # see Appendix B from VAE paper:
-    # Kingma and Welling. Auto-Encoding Variational Bayes. ICLR, 2014
-    # https://arxiv.org/abs/1312.6114
-    # 0.5 * sum(1 + log(sigma^2) - mu^2 - sigma^2)
-    KLD = -0.5 * torch.sum(1 + logvar - mu.pow(2) - logvar.exp())
-
-    return BCE + KLD
+        return BCE + KLD
 
 
-def train(epoch):
-    model.train()
-    train_loss = 0
-    for batch_idx, (data, _) in enumerate(train_loader):
-        data = data.to(device)
-        optimizer.zero_grad()
-        recon_batch, mu, logvar = model(data)
-        loss = loss_function(recon_batch, data, mu, logvar)
-        loss.backward()
-        train_loss += loss.item()
-        optimizer.step()
-        if batch_idx % args.log_interval == 0:
-            print('Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}'.format(
-                epoch, batch_idx * len(data), len(train_loader.dataset),
-                100. * batch_idx / len(train_loader),
-                loss.item() / len(data)))
-
-    print('====> Epoch: {} Average loss: {:.4f}'.format(
-          epoch, train_loss / len(train_loader.dataset)))
-
-def test(epoch):
-    model.eval()
-    test_loss = 0
-    with torch.no_grad():
-        for i, (data, _) in enumerate(test_loader):
+    def train(self, epoch):
+        self.model.train()
+        train_loss = 0
+        for batch_idx, (data, _) in enumerate(train_loader):
             data = data.to(device)
-            recon_batch, mu, logvar = model(data)
-            test_loss += loss_function(recon_batch, data, mu, logvar).item()
+            self.optimizer.zero_grad()
+            recon_batch, mu, logvar = self.model(data)
+            loss = self.loss_function(recon_batch, data, mu, logvar)
+            loss.backward()
+            train_loss += loss.item()
+            self.optimizer.step()
+            if batch_idx % args.log_interval == 0:
+                print('Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}'.format(
+                    epoch, batch_idx * len(data), len(train_loader.dataset),
+                    100. * batch_idx / len(train_loader),
+                    loss.item() / len(data)))
 
-    test_loss /= len(test_loader.dataset)
-    print('====> Test set loss: {:.4f}'.format(test_loss))
+        print('====> Epoch: {} Average loss: {:.4f}'.format(
+              epoch, train_loss / len(train_loader.dataset)))
+
+    def test(self, epoch):
+        self.model.eval()
+        test_loss = 0
+        with torch.no_grad():
+            for i, (data, _) in enumerate(test_loader):
+                data = data.to(device)
+                recon_batch, mu, logvar = self.model(data)
+                test_loss += self.loss_function(recon_batch, data, mu, logvar).item()
+
+        test_loss /= len(test_loader.dataset)
+        print('====> Test set loss: {:.4f}'.format(test_loss))
 
 
 if __name__ == "__main__":
+    vae = VAE()
     for epoch in range(1, args.epochs + 1):
-        train(epoch)
-        test(epoch)
+        vae.train(epoch)
+        vae.test(epoch)
         with torch.no_grad():
             images, _ = next(iter(test_loader))
             images = images.to(device)
             images_per_row = 20
-            interpolations = get_interpolations(model, device, images, images_per_row)
+            interpolations = get_interpolations(vae.model, device, images, images_per_row)
 
             sample = torch.randn(64, args.embedding_size).to(device)
-            sample = model.decode(sample).cpu()
+            sample = vae.model.decode(sample).cpu()
             save_image(sample.view(64, 1, 28, 28),
                     'results/sample_' + str(epoch) + '.png')
             save_image(interpolations.view(-1, 1, 28, 28),
